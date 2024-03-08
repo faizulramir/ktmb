d9cdad99f7ccc0c00961aabcc2849ca3af3b1e9d
Faizul Amir <faizul.ramir@gmail.com>
2024-03-08 22:08:43 +0800

    update

--- resources/js/Pages/Components/Detail.tsx
+++ resources/js/Pages/Components/Detail.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/Components/ui/sheet"
 import React, { useEffect } from "react";
 import { DetailData } from "./DetailData";
+import { Separator } from "@/Components/ui/separator"
 
 export function Detail(props:any) {
   const [open, setOpen] = React.useState(props.openDetail);
@@ -25,6 +26,13 @@ export function Detail(props:any) {
       <SheetContent>
         <SheetHeader>
           <SheetTitle>Train schedule.</SheetTitle>
+          <Separator className="my-2" />
+          <div className="grid grid-cols-3 font-bold">
+            <div>Trip No</div>
+            <div>Train No</div>
+            <div>Arrival Time</div>
+          </div>
+          <Separator className="my-2" />
           <DetailData moreDetail={props.moreDetail} key={resetKey}/>
         </SheetHeader>
       </SheetContent>
--- resources/js/Pages/Components/DetailData.tsx
+++ resources/js/Pages/Components/DetailData.tsx
@@ -15,24 +15,18 @@ export function DetailData(props:any) {
   return (
     <ScrollArea className="h-screen w-full rounded-md border pb-40">
       <div className="p-4">
-        <div className="grid grid-cols-3 font-bold">
-          <div>Trip No</div>
-          <div>Train No</div>
-          <div>Arrival Time</div>
-        </div>
-        <Separator className="my-2" />
-      {
-        props.moreDetail.map((train:any) => (
-          <div key={train.trainNo} ref={train.color ? paragraphRef : null}>
-            <div className={ train.color ? "grid grid-cols-3 bg-lime-300" : "grid grid-cols-3"}>
-              <div className={ !train.now ? "text-gray-400" : ""}>{train.tripNo}</div>
-              <div className={ !train.now ? "text-gray-400" : ""}>{train.trainNo}</div>
-              <div className={ !train.now ? "text-gray-400" : ""}>{train.timer}</div>
+        {
+          props.moreDetail.map((train:any) => (
+            <div key={train.trainNo} ref={train.color ? paragraphRef : null}>
+              <div className={ train.color ? "grid grid-cols-3 bg-lime-300" : "grid grid-cols-3"}>
+                <div className={ !train.now ? "text-gray-400" : ""}>{train.tripNo}</div>
+                <div className={ !train.now ? "text-gray-400" : ""}>{train.trainNo}</div>
+                <div className={ !train.now ? "text-gray-400" : ""}>{train.timer}</div>
+              </div>
+              <Separator className="my-2" />
             </div>
-            <Separator className="my-2" />
-          </div>
-        ))
-      }
+          ))
+        }
       </div>
     </ScrollArea>
   )
